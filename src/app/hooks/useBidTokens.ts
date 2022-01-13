74c71a8c740458c8b10925f5f5e1977ab509a5de
dmcong <83624415+dmcong@users.noreply.github.com>
2022-01-13 14:58:57 +0700

    update retailer watcher

---
+++ src/app/hooks/useBidTokens.ts
@@ -0,0 +1,27 @@
+import { AppState } from 'app/model'
+import { useCallback, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+
+export const useBidTokens = () => {
+  const { retailers } = useSelector((state: AppState) => state)
+  const [bidTokens, setBidTokens] = useState<string[]>([])
+
+  // Get all 'mint_bid' in list retailers data
+  const getBidTokens = useCallback(() => {
+    const mapHasTokens = new Map<string, boolean>()
+    const newBidTokens = []
+    for (const addr in retailers) {
+      const { mint_bid } = retailers[addr]
+      if (mapHasTokens.has(mint_bid)) continue
+      newBidTokens.push(mint_bid)
+      mapHasTokens.set(mint_bid, true)
+    }
+    return setBidTokens(newBidTokens)
+  }, [retailers])
+
+  useEffect(() => {
+    getBidTokens()
+  }, [getBidTokens])
+
+  return { bidTokens }
+}
