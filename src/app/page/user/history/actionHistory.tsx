2683fdd9f36ba8df76459c3a5e25f66f0128ac94
thiennv <thiennguyen@descartes.network>
2022-01-13 17:40:28 +0700

    _

--- src/app/page/user/history/actionHistory.tsx
+++ src/app/page/user/history/actionHistory.tsx
@@ -1,12 +1,53 @@
-import { Button } from 'antd'
+import { Button, Col, Modal, Row, Space, Typography } from 'antd'
+import { useState } from 'react'
+import IonIcon from 'shared/antd/ionicon'
 import { OrderStatus } from './orderHisotryStatus'
 
 const ActionHistory = ({ state = 0 }: { state?: number }) => {
+  const [visible, setVisible] = useState(false)
+
   if (state === OrderStatus.Pending)
     return (
-      <Button size="small" ghost onClick={() => {}} block>
-        Cancel
-      </Button>
+      <Row>
+        <Col span={24}>
+          <Button size="small" onClick={() => setVisible(true)} block>
+            Cancel
+          </Button>
+          <Modal
+            visible={visible}
+            onCancel={() => {}}
+            footer={null}
+            closable={false}
+          >
+            <Row gutter={[32, 32]} justify="end">
+              <Col span={24}>
+                <Space size={16} align="start">
+                  <Typography.Title level={4} style={{ color: '#FA8C16' }}>
+                    <IonIcon name="alert-circle-outline" />
+                  </Typography.Title>
+                  <Space size={4} direction="vertical">
+                    <Typography.Title level={5}>
+                      Do you want to cancel this order?
+                    </Typography.Title>
+                    <Typography.Text>
+                      You will still pay the network fee when you cancel the
+                      order.
+                    </Typography.Text>
+                  </Space>
+                </Space>
+              </Col>
+              <Col>
+                <Space>
+                  <Button onClick={() => setVisible(false)}>No</Button>
+                  <Button type="primary" onClick={() => {}}>
+                    Yes
+                  </Button>
+                </Space>
+              </Col>
+            </Row>
+          </Modal>
+        </Col>
+      </Row>
     )
   if (state === OrderStatus.Success)
     return (
