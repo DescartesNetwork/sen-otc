ae414dbb16eb04ec026387fa7d7b51b407de1ea3
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-01-14 14:14:08 +0700

    Remove Row in price.tsx

--- src/app/components/price.tsx
+++ src/app/components/price.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux'
 import { utils } from '@senswap/sen-js'
 
-import { Row, Space, Typography } from 'antd'
+import { Space, Typography } from 'antd'
 import { AppState } from 'app/model'
 import IonIcon from 'shared/antd/ionicon'
 import { MintAvatar, MintSymbol } from 'shared/antd/mint'
@@ -13,21 +13,19 @@ const Price = ({ orderId }: { orderId: string }) => {
   } = useSelector((state: AppState) => state)
   const retailerData = retailers[orderData.retailer]
   return (
-    <Row>
-      <Space size={6}>
-        <Typography.Text>
-          {utils.undecimalize(BigInt(orderData.ask_amount), 0)}
-        </Typography.Text>
-        <MintSymbol mintAddress={retailerData.mint_bid} />
-        <MintAvatar mintAddress={retailerData.mint_bid} />
-        <IonIcon name="arrow-forward-outline" />
-        <Typography.Text>
-          {utils.undecimalize(BigInt(orderData.bid_amount), 0)}
-        </Typography.Text>
-        <MintSymbol mintAddress={retailerData.mint_ask} />
-        <MintAvatar mintAddress={retailerData.mint_ask} />
-      </Space>
-    </Row>
+    <Space size={6}>
+      <Typography.Text>
+        {utils.undecimalize(BigInt(orderData.ask_amount), 0)}
+      </Typography.Text>
+      <MintSymbol mintAddress={retailerData.mint_bid} />
+      <MintAvatar mintAddress={retailerData.mint_bid} />
+      <IonIcon name="arrow-forward-outline" />
+      <Typography.Text>
+        {utils.undecimalize(BigInt(orderData.bid_amount), 0)}
+      </Typography.Text>
+      <MintSymbol mintAddress={retailerData.mint_ask} />
+      <MintAvatar mintAddress={retailerData.mint_ask} />
+    </Space>
   )
 }
 
