cb988096d5f8f3376b09d1b4737ead3e3110fb45
thiennv <thiennguyen@descartes.network>
2022-01-18 00:38:23 +0700

    mobile history

--- src/app/components/orderPrice.tsx
+++ src/app/components/orderPrice.tsx
@@ -1,26 +1,10 @@
 import { useSelector } from 'react-redux'
-import { utils } from '@senswap/sen-js'
 
-import { Space, Typography } from 'antd'
+import { Space } from 'antd'
 import { AppState } from 'app/model'
 import IonIcon from 'shared/antd/ionicon'
-import { MintAvatar, MintSymbol } from 'shared/antd/mint'
-
-const PriceCell = ({
-  amount,
-  mintAddress,
-}: {
-  amount: bigint
-  mintAddress: string
-}) => {
-  return (
-    <Space>
-      <Typography.Text>{utils.undecimalize(amount, 0)}</Typography.Text>
-      <MintSymbol mintAddress={mintAddress} />
-      <MintAvatar mintAddress={mintAddress} />
-    </Space>
-  )
-}
+import { MintAvatar } from 'shared/antd/mint'
+import OrderPriceCell from './orderPriceCell'
 
 const Price = ({ orderId }: { orderId: string }) => {
   const {
@@ -28,18 +12,16 @@ const Price = ({ orderId }: { orderId: string }) => {
     retailers,
   } = useSelector((state: AppState) => state)
   const retailerData = retailers[orderData.retailer]
+  const { bid_amount, ask_amount } = orderData
+  const { mint_ask, mint_bid } = retailerData
 
   return (
     <Space>
-      <PriceCell
-        amount={orderData.ask_amount}
-        mintAddress={retailerData.mint_ask}
-      />
+      <OrderPriceCell amount={ask_amount} mintAddress={mint_ask} />
+      <MintAvatar mintAddress={mint_bid} />
       <IonIcon name="arrow-forward-outline" />
-      <PriceCell
-        amount={orderData.bid_amount}
-        mintAddress={retailerData.mint_bid}
-      />
+      <MintAvatar mintAddress={mint_ask} />
+      <OrderPriceCell amount={bid_amount} mintAddress={mint_bid} />
     </Space>
   )
 }
