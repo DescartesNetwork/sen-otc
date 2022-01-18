01fc36cb7bde711ceaa6e49d3325d22a1af138dd
thiennv <thiennguyen@descartes.network>
2022-01-18 21:06:40 +0700

    ui widget

--- src/app/components/orderPriceCell.tsx
+++ src/app/components/orderPriceCell.tsx
@@ -2,6 +2,7 @@ import { utils } from '@senswap/sen-js'
 
 import { Space, Typography } from 'antd'
 import { MintSymbol } from 'shared/antd/mint'
+import useMintDecimals from 'shared/hooks/useMintDecimals'
 
 const OrderPriceCell = ({
   amount,
@@ -10,9 +11,12 @@ const OrderPriceCell = ({
   amount: bigint
   mintAddress: string
 }) => {
+  const decimals = useMintDecimals(mintAddress)
+  const amountValue = decimals ? utils.undecimalize(amount, decimals) : 0
+
   return (
     <Space>
-      <Typography.Text>{utils.undecimalize(amount, 0)}</Typography.Text>
+      <Typography.Text>{amountValue}</Typography.Text>
       <Typography.Text type="secondary">
         <MintSymbol mintAddress={mintAddress} />
       </Typography.Text>
--- src/app/widget/index.tsx
+++ src/app/widget/index.tsx
@@ -1,12 +1,40 @@
+// import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
 import { Row, Col, Typography } from 'antd'
+import OrderCard from 'app/page/user/history/orderCard'
+import { AppState } from 'app/model'
+import Watcher from 'app/components/watcher'
+// import CoinFilterHistory from 'app/components/filterHistory/coinFilterHistory'
 
 const Widget = () => {
+  // const [selectedCoin, setSelectedCoin] = useState('')
+  const { orders } = useSelector((state: AppState) => state)
+
   return (
-    <Row gutter={[24, 24]}>
-      <Col span={24}>
-        <Typography.Text>Widget</Typography.Text>
-      </Col>
-    </Row>
+    <Watcher>
+      <Row gutter={[24, 24]}>
+        <Col span={24}>
+          <Row gutter={[16, 16]}>
+            <Col flex="auto">
+              <Typography.Text>5 Order</Typography.Text>
+            </Col>
+            <Col>
+              {/* <CoinFilterHistory orderState={selectedCoin}  onSelect={setSelectedCoin}/> */}
+            </Col>
+          </Row>
+        </Col>
+        <Col span={24}>
+          <Row gutter={[24, 24]}>
+            {Object.keys(orders)?.map((address) => (
+              <Col span={24} key={address}>
+                <OrderCard orderId={address} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    </Watcher>
   )
 }
 
