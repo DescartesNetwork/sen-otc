bf2dd1cf98e2c7880b497d5beecade2e97f14e58
dmcong <83624415+dmcong@users.noreply.github.com>
2022-01-20 15:32:44 +0700

    refactor useBidMint

--- src/app/hooks/useBidMints.ts
+++ src/app/hooks/useBidMints.ts
@@ -1,28 +1,25 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useMint } from '@senhub/providers'
 
 import { AppState } from 'app/model'
 import { useSortedMint } from './useSortedMint'
-import { useMint } from '@senhub/providers'
 
 export const useBidMints = () => {
-  const { retailers } = useSelector((state: AppState) => state)
   const [bidMints, setBidMints] = useState<string[]>([])
+  const { retailers } = useSelector((state: AppState) => state)
   const { sortedMints } = useSortedMint(bidMints)
   const { tokenProvider } = useMint()
 
   // Get all 'mint_bid' in list retailers data
   const getBidMints = useCallback(async () => {
-    const mapHasMints = new Map<string, boolean>()
-    const newBidMints = []
+    const mapMints: Record<string, boolean> = {}
     for (const addr in retailers) {
       const { mint_bid } = retailers[addr]
       const tokenInfo = await tokenProvider.findByAddress(mint_bid)
-      if (mapHasMints.has(mint_bid) || !tokenInfo) continue
-      newBidMints.push(mint_bid)
-      mapHasMints.set(mint_bid, true)
+      if (tokenInfo) mapMints[mint_bid] = true
     }
-    return setBidMints(newBidMints)
+    return setBidMints(Object.keys(mapMints))
   }, [retailers, tokenProvider])
 
   useEffect(() => {
