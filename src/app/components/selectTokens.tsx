8edbba0f5daa30c273830814df2a5e879e5fccd7
dmcong <83624415+dmcong@users.noreply.github.com>
2022-01-20 17:34:05 +0700

    Update selectTokens.tsx

--- src/app/components/selectTokens.tsx
+++ src/app/components/selectTokens.tsx
@@ -3,7 +3,43 @@ import { useState } from 'react'
 import { Button, Card, Col, Input, Row, Select, SelectProps, Space } from 'antd'
 import IonIcon from 'shared/antd/ionicon'
 import { MintAvatar, MintSymbol } from 'shared/antd/mint'
+
 import { useSearchTokens } from 'app/hooks/useSearchTokens'
+
+export const SearchInput = ({
+  value,
+  onChange,
+}: {
+  value: string
+  onChange: (val: string) => void
+}) => {
+  return (
+    <Card
+      className="account-card"
+      style={{ borderRadius: 8, margin: 8, boxShadow: 'unset' }}
+      bodyStyle={{ padding: 6 }}
+    >
+      <Input
+        placeholder="Search"
+        value={value}
+        size="small"
+        bordered={false}
+        prefix={
+          <Button
+            type="text"
+            style={{ marginLeft: -7 }}
+            size="small"
+            onClick={value ? () => onChange('') : undefined}
+            icon={<IonIcon name={value ? 'close-outline' : 'search-outline'} />}
+          />
+        }
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          onChange(e.target.value)
+        }
+      />
+    </Card>
+  )
+}
 
 const TokenSelect = ({
   tokens,
@@ -24,34 +60,7 @@ const TokenSelect = ({
         <Row gutter={[8, 8]}>
           {search && (
             <Col span={24}>
-              <Card
-                className="account-card"
-                style={{ borderRadius: 8, margin: 8, boxShadow: 'unset' }}
-                bodyStyle={{ padding: 6 }}
-              >
-                <Input
-                  placeholder="Search"
-                  value={keyword}
-                  size="small"
-                  bordered={false}
-                  prefix={
-                    <Button
-                      type="text"
-                      style={{ marginLeft: -7 }}
-                      size="small"
-                      onClick={keyword ? () => setKeyword('') : undefined}
-                      icon={
-                        <IonIcon
-                          name={keyword ? 'close-outline' : 'search-outline'}
-                        />
-                      }
-                    />
-                  }
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                    setKeyword(e.target.value)
-                  }
-                />
-              </Card>
+              <SearchInput value={keyword} onChange={setKeyword} />
             </Col>
           )}
           <Col span={24}>{menu}</Col>
