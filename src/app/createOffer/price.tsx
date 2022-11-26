08ec4bbb732fab3514b8ed3ee14d822cb6a61f51
tuphan-dn <tuphan@descartes.network>
2022-11-26 21:07:09 +0700

    price

--- src/app/createOffer/price.tsx
+++ src/app/createOffer/price.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 
-import { Col, Row, Space, Typography } from 'antd'
+import { Card, Col, Divider, Row, Space, Typography } from 'antd'
 
 import configs from 'configs'
 import { numeric } from 'helpers/util'
@@ -18,12 +18,9 @@ const {
   otc: { partneredTokens },
 } = configs
 
-const Price = () => {
+const ReferencePrice = () => {
   const { bidToken } = useBidToken()
   const { askToken } = useAskToken()
-  const { askPrice } = useAskPrice()
-  const { bidAmount } = useBidAmount()
-  const { askAmount } = useAskAmount()
   const { mode } = useMode()
   const { price: referencePrice } = usePrice(
     partneredTokens.find(
@@ -31,29 +28,112 @@ const Price = () => {
     )?.ticket || '',
   )
 
-  const offeredPrice = useMemo(() => {
-    if (askPrice) return askPrice
-    if (mode === 'Buy') return Number(askAmount) / Number(bidAmount)
-    return Number(bidAmount) / Number(askAmount)
-  }, [askPrice, mode, bidAmount, askAmount])
+  const referenceUnit = useMemo(() => {
+    if (mode === 'Buy') return askToken
+    else return bidToken
+  }, [mode, bidToken, askToken])
 
   return (
-    <Row gutter={[8, 8]}>
-      <Col span={12}>
-        <Space direction="vertical">
-          <Typography.Title level={2}>
-            ${numeric(referencePrice).format('0,0.[0000]')}
-          </Typography.Title>
-          <Typography.Text type="secondary">Reference Price</Typography.Text>
-        </Space>
+    <Space direction="vertical">
+      <Typography.Title level={2}>
+        ${numeric(referencePrice).format('0,0.[0000]')}
+      </Typography.Title>
+      <Space size={0}>
+        <Typography.Text type="secondary">Reference Price</Typography.Text>
+        <Divider type="vertical" />
+        <Typography.Text type="secondary">{referenceUnit}</Typography.Text>
+      </Space>
+    </Space>
+  )
+}
+
+const OfferedPrice = () => {
+  const { bidToken } = useBidToken()
+  const { askToken } = useAskToken()
+  const { askPrice } = useAskPrice()
+  const { bidAmount } = useBidAmount()
+  const { askAmount } = useAskAmount()
+  const { mode } = useMode()
+
+  const offeredPrice = useMemo(() => {
+    if (askPrice) return Number(askPrice)
+    if (!bidAmount || !Number(bidAmount) || !askAmount || !Number(askAmount))
+      return 0
+    if (mode === 'Buy') return Number(bidAmount) / Number(askAmount)
+    return Number(askAmount) / Number(bidAmount)
+  }, [askPrice, mode, bidAmount, askAmount])
+
+  const offeredUnit = useMemo(() => {
+    if (mode === 'Buy') return askToken
+    else return bidToken
+  }, [mode, bidToken, askToken])
+
+  return (
+    <Space direction="vertical">
+      <Typography.Title level={2}>
+        ${numeric(offeredPrice).format('0,0.[0000]')}
+      </Typography.Title>
+      <Space size={0}>
+        <Typography.Text type="secondary">Offered Price</Typography.Text>
+        <Divider type="vertical" />
+        <Typography.Text type="secondary">{offeredUnit}</Typography.Text>
+      </Space>
+    </Space>
+  )
+}
+
+const OfferedAmount = () => {
+  const { bidToken } = useBidToken()
+  const { askToken } = useAskToken()
+  const { askPrice } = useAskPrice()
+  const { bidAmount } = useBidAmount()
+  const { askAmount } = useAskAmount()
+  const { mode } = useMode()
+
+  const offeredAmount = useMemo(() => {
+    if (askAmount) return Number(askAmount)
+    if (!askPrice || !Number(askPrice) || !bidAmount || !Number(bidAmount))
+      return 0
+    if (mode === 'Buy') return Number(bidAmount) / Number(askPrice)
+    return Number(askPrice) * Number(bidAmount)
+  }, [askPrice, mode, bidAmount, askAmount])
+
+  const offeredUnit = useMemo(() => {
+    if (mode === 'Buy') return askToken
+    else return bidToken
+  }, [mode, bidToken, askToken])
+
+  return (
+    <Space direction="vertical">
+      <Typography.Title level={2}>
+        {numeric(offeredAmount).format('0,0.[0000]')}
+      </Typography.Title>
+      <Space size={0}>
+        <Typography.Text type="secondary">Offered Amount</Typography.Text>
+        <Divider type="vertical" />
+        <Typography.Text type="secondary">{offeredUnit}</Typography.Text>
+      </Space>
+    </Space>
+  )
+}
+
+const Price = () => {
+  return (
+    <Row gutter={[12, 12]}>
+      <Col xs={24} md={12}>
+        <Card>
+          <ReferencePrice />
+        </Card>
       </Col>
-      <Col span={12}>
-        <Space direction="vertical">
-          <Typography.Title level={2}>
-            ${numeric(offeredPrice).format('0,0.[0000]')}
-          </Typography.Title>
-          <Typography.Text type="secondary">Offered Price</Typography.Text>
-        </Space>
+      <Col xs={24} md={12}>
+        <Card>
+          <OfferedPrice />
+        </Card>
+      </Col>
+      <Col span={24}>
+        <Card>
+          <OfferedAmount />
+        </Card>
       </Col>
     </Row>
   )
